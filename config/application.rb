9495ab27d255b97d6278ae27c31d2254b763438b
juli212 <jjgiraldi@gmail.com>
2018-06-04 00:08:45 -0400

    update cors origins

--- config/application.rb
+++ config/application.rb
@@ -33,7 +33,8 @@ module PubgamerApi
 
     config.middleware.insert_before 0, Rack::Cors do
         allow do
-            origins 'http://localhost:3000'
+            origins 'http://pubgamr.herokuapp.com'
+            # origins 'http://localhost:3000'
             resource '*', :headers => :any, :methods => [:get, :post, :put, :delete, :options]
         end
     end
