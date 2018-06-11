8a78727994776c9d17b1f7d7caaf05077d3c9a4f
juli212 <jjgiraldi@gmail.com>
2018-06-11 14:49:04 -0400

    replace hardcoded user id with current_user id on create

--- app/controllers/api/v1/reviews_controller.rb
+++ app/controllers/api/v1/reviews_controller.rb
@@ -1,13 +1,10 @@
 # require 'will_paginate/array'
 module Api::V1
   class ReviewsController < BaseController
-  	# before_action :authenticate_user!, except: [:index, :create, :new]
-  	# wrap_parameters :venue, include: [:name, :address, :lat, :lng, :neighborhood, :games]
-  	skip_before_action :require_login!, only: [:index, :create, :show]
+  	skip_before_action :require_login!, only: [:index]
 
     def index
     	reviews = Review.where(venue_id: params[:venue_id])
-    	# binding.pry
       render json: reviews,
       	only: [:id, :content, :rating, :day, :user_id],
       	methods: [:numerical_date],
@@ -19,13 +16,12 @@ module Api::V1
 	  def create
 	  	review = Review.new(review_params)
 	  	review.venue = Venue.find_by(id: params[:venue_id])
-	  	review.user_id = 1
+	  	review.user_id = current_user.id
 	  	if review.save
 	  		params[:vibes].each do |v|
 	  			review.vibes << Vibe.find_by(name: v)
 	  		end
 	  	end
-	  	# binding.pry
 	  	render json: review,
 	  		only: [:id, :content, :rating, :day, :user_id],
 	  		methods: [:numerical_date],
